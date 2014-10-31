ec331af85df7a82748976e42d661441d83e960c9
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2014-10-31 16:13:42 -0700

    init commit

--- Stack and Queue/minimumExpression.py
+++ Stack and Queue/minimumExpression.py
@@ -5,15 +5,17 @@ class Solution:
       if n == 1: return [1]
       queue = deque()
       queue.append([1])
+      res = [1] * n
       while queue:
          cur = queue.popleft()
          for num in cur:
             sum = num + cur[-1]
             if sum > n: break
-            next = cur
-            next.append(sum)
-            if sum == n: return next
+            next = cur + [sum]
+            if sum == n and len(next) < len(res):
+               res = next
             queue.append(next)
+      return res
 
 if __name__ == "__main__":
    t = Solution()
