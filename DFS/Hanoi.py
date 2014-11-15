f7cbef187daea715e8da06c19e1fed82f5101fba
RUICHEN MA <ruichenma@RUICHENs-MacBook-Pro.local>
2014-11-14 16:22:12 -0800

    init commit

--- DFS/Hanoi.py
+++ DFS/Hanoi.py
@@ -1,20 +1,24 @@
 class Solution:
    # Time: O(2^n)
    # Space: O(n)
-   def hanoi(self, n):
-      p1, p2, p3 = [i+1 for i in range(n)], [], []
+   def hanoi(self, n, p1, p2, p3):
       return self.hanoiHelper(n, p1, p2, p3)
 
    def hanoiHelper(self, n, src, dst, use):
-      if n > 1:
+      if n == 1:
+         dst.append(src.pop())
+         return 1
+      else:
          count = self.hanoiHelper(n-1, src, use, dst)
          dst.append(src.pop())
          count += 1
-         count += self.hanoiHelper(n-1, dst, use, src)
+         count += self.hanoiHelper(n-1, use, dst, src)
          return count
-      return 1
 
 if __name__ == "__main__":
    t = Solution()
    n = 4
-   print t.hanoi(n)
+   p1, p2, p3 = [i+1 for i in range(n)], [], []
+   print p1, p2, p3
+   print t.hanoi(n, p1, p2, p3)
+   print p1, p2, p3
